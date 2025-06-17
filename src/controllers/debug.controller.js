9cd5b8a29451fd49e9572e48a2e8717fa9f49b07
jeancdt <flwss157@gmail.com>
2025-06-17 20:24:23 -0300

    update: remoção de console.logs nos arquivos debug

--- src/controllers/debug.controller.js
+++ src/controllers/debug.controller.js
@@ -3,7 +3,6 @@ const UserService = require("../services/userService");
 class DebugController {
   // Cadastrar novo usuário
   static async main(req, res) {
-    console.log("aloooooooo")
     return res.status(201).json('{"hello":"world"}');
   }
 }
--- src/routes/debug.routes.js
+++ src/routes/debug.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const DebugController = require('../controllers/debug.controller');
 const router = express.Router();
 
-console.log(`aloooooooooooooooooooooooooooooooooooooooooo`)
 router.get('/main', DebugController.main);
 
 module.exports = router;
