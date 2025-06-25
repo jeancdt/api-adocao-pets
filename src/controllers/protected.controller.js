c5266604100665f75b56c41ab5bba6fd3f814fed
jeancdt <jeancdetoni@gmail.com>
2025-06-24 21:08:35 -0300

    update: funções (getUsers, findById, updateUser e deleteById) protected controller

--- src/controllers/protected.controller.js
+++ src/controllers/protected.controller.js
@@ -1,3 +1,5 @@
+const UserService = require("../services/userService");
+
 class ProtectedController {
     // Usuário normal
     static dashboard(req, res) {
@@ -24,6 +26,66 @@ class ProtectedController {
         } catch (error) {
             return res.status(500).json({
                 message: 'Erro ao acessar a área admin',
+                error: error.message
+            });
+        }
+    }
+
+    static async getUsers(req, res) {
+        try {
+            const { users } = await UserService.getUsers();
+
+            return res.status(200).json(users);
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Erro ao buscar usuários',
+                error: error.message
+            });
+        }
+    }
+
+    static async findById(req, res) {
+        try {
+            const id = req.params.id;
+            const { user } = await UserService.findById(id);
+
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Erro ao buscar usuário por ID',
+                error: error.message
+            });
+        }
+    }
+
+    // Atualiza dados do usuário pelo ID
+    static async updateUser(req, res) {
+        try {
+            const id = Number(req.params.id);
+            const data = req.body;
+
+            const result = await UserService.updateUser(id, data);
+
+            return res.status(200).json(result);
+        } catch (error) {
+            const status = error.status || 500;
+
+            return res.status(status).json({
+                message: error.message || 'Erro ao atualizar usuário',
+            });
+        }
+    }
+
+    // Remove um usuário por ID
+    static async deleteById(req, res) {
+        try {
+            const id = req.params.id;
+            const result = await UserService.deleteById(id);
+
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(error.status || 500).json({
+                message: error.message || 'Erro ao remover usuário',
                 error: error.message
             });
         }
