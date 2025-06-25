b25febca46232ffa7d344ddc7187836a09cfa921
jeancdt <jeancdetoni@gmail.com>
2025-06-24 21:09:03 -0300

    update: rotas (getUsers, findById, updateUser e deleteById) protected routes

--- src/routes/protected.routes.js
+++ src/routes/protected.routes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router.get('/dashboard', authenticateToken, ProtectedController.dashboard);
 router.get('/admin', authenticateToken, authorizeRole('admin'), ProtectedController.adminOnly);
+router.get('/users', authenticateToken, authorizeRole('admin'), ProtectedController.getUsers);
+router.get('/users/:id', authenticateToken, ProtectedController.findById);
+router.put('/users/:id', authenticateToken, ProtectedController.updateUser);
+router.delete('/users/:id', authenticateToken, authorizeRole('admin'), ProtectedController.deleteById);
 
 module.exports = router;
